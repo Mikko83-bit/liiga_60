519c8603bbf92566c2fb399355c45b4c8efac050
Mikko83-bit <mikko.kuikka38@gmail.com>
2026-05-19 11:48:36 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Liiga Analytics",
+    page_icon="🏒",
+    layout="wide"
+)
+
+st.title("🏒 Liiga Analytics")
+
+st.write("Choose a page from sidebar.")
