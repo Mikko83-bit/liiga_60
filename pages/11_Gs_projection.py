4ce1f91c1eb7382700dc4657223596f262323e83
Mikko83-bit <mikko.kuikka38@gmail.com>
2026-05-20 15:01:23 +0300

    Update 11_Gs_projection.py

--- pages/11_Gs_projection.py
+++ pages/11_Gs_projection.py
@@ -30,7 +30,7 @@ Projection-oriented player model using:
 # LOAD EXCEL
 # =========================================================
 
-FILE = "data/Liiga 2025-2026_skaters_teams.xlsx"
+FILE = "Liiga 2025-2026_skaters_teams.xlsx"
 
 # ---------------------------------------------------------
 # PLAYER SHEET
